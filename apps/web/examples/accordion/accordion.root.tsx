0b6424597e33b7bd9154f51ad92a59c941f4915a
Aditya suryavanshi <167076950+Adityasuryavanshi23@users.noreply.github.com>
2025-01-30 16:56:31 +0530

    Update accordion.root.tsx

--- apps/web/examples/accordion/accordion.root.tsx
+++ apps/web/examples/accordion/accordion.root.tsx
@@ -57,7 +57,7 @@ export function Component() {
             technical reason stopping you from using the best of two worlds.
           </p>
           <p className="mb-2 text-gray-500 dark:text-gray-400">Learn more about these technologies:</p>
-          <ul className="list-disc pl-5 text-gray-500 dark:text-gray-400">
+          <ul className="list-disc pl-2 text-gray-500 dark:text-gray-400">
             <li>
               <a href="https://flowbite.com/pro/" className="text-cyan-600 hover:underline dark:text-cyan-500">
                 Flowbite Pro
